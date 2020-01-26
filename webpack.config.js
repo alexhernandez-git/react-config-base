535ca00b71a6580450fea1d37104c93e2ec125dd
Alex Hernandez <vlexhndz@gmail.com>
2020-01-26 11:00:04 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+// Requerir path
+const path = require('path');
+// Requerir HTML plugin 
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+// Requerir CSS plugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node-modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader, 
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css'
+        })
+    ]
+}
